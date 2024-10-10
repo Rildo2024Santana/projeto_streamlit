5f8d15703884b7668b226927ff5fcc07644b7d0c
Rildo2024Santana <rildosantana@gmail.com>
2024-10-10 20:34:15 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -5,7 +5,7 @@ from datetime import date
 def gravar_dados(nome,dt_cadas,tipo,endereco,numero,ponto,tel,celular):
     if nome and dt_cadas <= date.today():
         with open("clientes.csv", "a", encoding="utf-8") as file:
-            file.write(f"{nome},{dt_cadas},{tipo}\n,{endereco},{numero},{ponto},{tel},{celular}")
+            file.write(f"{nome},{endereco},{numero},{ponto},{tel},{celular},{dt_cadas},{tipo}\n")
         st.session_state["sucesso"] = True
     else:
         st.session_state["sucesso"] = False   
@@ -45,7 +45,7 @@ tipo =st.selectbox("Tipo do cliente",
 
 btn_cadastrar = st.button("Cadastrar", 
                           on_click=gravar_dados,
-                          args=[nome, dt_cadas, tipo, endereco, numero, ponto_ref, tel, celular,])
+                          args=[nome, endereco, numero, ponto_ref, tel, celular,dt_cadas, tipo,])
 
 
 if btn_cadastrar:
