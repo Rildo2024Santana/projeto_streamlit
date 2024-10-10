5654e15e12bf5a4b665422063e7004b89bd16821
Rildo2024Santana <rildosantana@gmail.com>
2024-10-10 20:37:23 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 
-def gravar_dados(nome,dt_cadas,tipo,endereco,numero,ponto,tel,celular):
+def gravar_dados(nome,endereco,numero,ponto_ref,tel,celular,dt_cadas,tipo,):
     if nome and dt_cadas <= date.today():
         with open("clientes.csv", "a", encoding="utf-8") as file:
-            file.write(f"{nome},{endereco},{numero},{ponto},{tel},{celular},{dt_cadas},{tipo}\n")
+            file.write(f"{nome},{endereco},{numero},{ponto_ref},{tel},{celular},{dt_cadas},{tipo}\n")
         st.session_state["sucesso"] = True
     else:
         st.session_state["sucesso"] = False   
@@ -23,13 +23,13 @@ nome = st.text_input("Nome do Cliente",
                       key="nome_cliente")
 
 endereco = st.text_input("Endereço",
-                          key="endereco")
+                          key="endereco_cliente")
                  
 numero = st.text_input("Número",
                       key="numero")
 
 ponto_ref = st.text_input("Ponto de Referência",
-                         key="ponto_referencia")
+                         key="ponto_ref")
 
 tel = st.text_input("Telefone Contato",
                    key="telefone_contato")
