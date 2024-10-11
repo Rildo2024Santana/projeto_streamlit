a64f03446bb120c4937a613c762d744862fd4a8e
Rildo2024Santana <rildosantana@gmail.com>
2024-10-11 17:17:15 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -2,38 +2,40 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 
-def gravar_dados(nome,endereco,data_cadas,tipo):
+
+def grava_dados(nome, endereco,data_cadas, tipo):
     if nome and data_cadas <= date.today():
         with open("clientes.csv", "a", encoding="utf-8") as file:
-            file.write(f"{nome},{endereco},{data_cadas},{tipo}\n")
+            file.write(f"{nome},{endereco},{data_cadas},{tipo}")
         st.session_state["sucesso"] = True
     else:
-        st.session_state["sucesso"] = False   
-    
+        st.session_state["sucesso"] = False
 
-st.set_page_config(
-    page_title="Cadastro de Clientes",
-    page_icon="🗃️"
+
+st.st_page_config(
+    page_title="Cadastro de Cliente",
+    page_icon="📕"
 )
 
-st.title("Cadastro de clientes")
+
+st.title("Cadastro de Cliente")
 st.divider()
 
-nome = st.text_input("Nome do cliente",
-                      key="nome_cliente")
+
+nome = st.text_input("Nome do Cliente",
+                     key="nome_cliente")
 
 endereco = st.text_input("Endereço",
-                   key="endereco")
-                     
-dt_cadas = st.date_input("Data Cadastro", format="DD/MM/YYYY")
+                         key="endereco")
 
-tipo =st.selectbox("Tipo do cliente",
-                   ["","Pessoa jurídica", "Pessoa física"])
+dt_cadas = st.date_input ("Dta do Cadastro", format="DD/MM/YYYY")
 
-btn_cadastrar = st.button("Cadastrar", 
-                          on_click=gravar_dados,
-                          args=[nome, end, dt_cadas, tipo])
+tipo = st.selectbox("Tipo do Cliente",
+                    [" ", "Pessoa jurídica", "Pessoa física"])
 
+btn_cadastrar = st.buttom("Cadastrar", 
+                          om_click= grava_dados,
+                          args=[nome,endereco,dt_cadas,tipo])
 
 if btn_cadastrar:
     if st.session_state["sucesso"]:
@@ -41,6 +43,6 @@ if btn_cadastrar:
                    icon="✅")
     else:
         st.error("Houve algum problema no cadastro!",
-                icon="❌")
+                 icon="❌") 
     
  
