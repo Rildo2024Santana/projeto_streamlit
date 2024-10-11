e9e4883e8990cdedba8591a102694e9c1c39b6ad
Rildo2024Santana <rildosantana@gmail.com>
2024-10-11 16:18:01 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 
-def gravar_dados(nome,end,data_cadas,tipo):
+def gravar_dados(nome,endereco,data_cadas,tipo):
     if nome and data_cadas <= date.today():
         with open("clientes.csv", "a", encoding="utf-8") as file:
-            file.write(f"{nome},{end},{data_cadas},{tipo}\n")
+            file.write(f"{nome},{endereco},{data_cadas},{tipo}\n")
         st.session_state["sucesso"] = True
     else:
         st.session_state["sucesso"] = False   
@@ -22,8 +22,8 @@ st.divider()
 nome = st.text_input("Nome do cliente",
                       key="nome_cliente")
 
-end = st.text_input("Endereço",
-                   key="end")
+endereco = st.text_input("Endereço",
+                   key="endereco")
                      
 dt_cadas = st.date_input("Data Cadastro", format="DD/MM/YYYY")
 
