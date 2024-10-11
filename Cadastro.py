ef31ff9a1942f26ecb1c71336920555a50f17319
Rildo2024Santana <rildosantana@gmail.com>
2024-10-11 18:44:51 -0300

    Update Cadastro.py

--- Cadastro.py
+++ Cadastro.py
@@ -2,37 +2,33 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 
-
-def grava_dados(nome, endereco,data_cadas, tipo):
-    if nome and data_cadas <= date.today():
-        with open("clientes.csv", "a", encoding="utf-8") as file:
-            file.write(f"{nome},{endereco},{data_cadas},{tipo}")
+def gravar_dados(nome,data_nasc,tipo):
+    if nome and data_nasc <= date.today():
         st.session_state["sucesso"] = True
     else:
-        st.session_state["sucesso"] = False
+        st.session_state["sucesso"] = False   
+    
 
-st.st_page_config(
+st.set_page_config(
     page_title="Cadastro de Clientes",
-    page_icon="📕"
+    page_icon="🗃️"
 )
 
-st.title("Cadastro de Clientes")
+st.title("Cadastro de clientes")
 st.divider()
 
-nome = st.text_input("Nome do Cliente",
-                     key="nome_cliente")
+nome = st.text_input("Digite o nome do cliente",
+                      key="nome_cliente")
 
-endereco = st.text_input("Endereço",
-                         key="endereco")
+dt_nasc = st.date_input("Data nascimento", format="DD/MM/YYYY")
 
-dt_cadas = st.date_input ("Dta do Cadastro", format="DD/MM/YYYY")
+tipo =st.selectbox("Tipo do cliente",
+                   ["Pessoa jurídica", "Pessoa física"])
 
-tipo = st.selectbox("Tipo do Cliente",
-                    [" ", "Pessoa jurídica", "Pessoa física"])
+btn_cadastrar = st.button("Cadastrar", 
+                          on_click=gravar_dados,
+                          args=[nome, dt_nasc, tipo])
 
-btn_cadastrar = st.buttom("Cadastrar", 
-                          om_click= grava_dados,
-                          args=[nome,endereco,dt_cadas,tipo])
 
 if btn_cadastrar:
     if st.session_state["sucesso"]:
@@ -40,4 +36,4 @@ if btn_cadastrar:
                    icon="✅")
     else:
         st.error("Houve algum problema no cadastro!",
-                 icon="❌")    
+                icon="❌")
