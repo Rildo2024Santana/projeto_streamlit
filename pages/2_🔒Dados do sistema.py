fa1fec756f05fe8e7b9cb70ba1204b3768506f2b
Rildo2024Santana <rildosantana@gmail.com>
2024-10-16 20:06:56 -0300

    Update 2_🔒Dados do sistema.py

--- pages/2_🔒Dados do sistema.py
+++ pages/2_🔒Dados do sistema.py
@@ -4,9 +4,5 @@ st.set_page_config(
     page_title="Dados do Sistema",
     page_icon="♟️")
 
-## cidade = st.session_state["cidade"]
 
-## st.title("Dados do Sistema")
-## st.text(st.session_state["data"])
-## st.text(st.session_state["cidade"])
 
