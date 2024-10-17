6ab4df7dd3bee1bbb301c400046771288cb308a0
Rildo2024Santana <rildosantana@gmail.com>
2024-10-17 19:53:59 -0300

    Update 2_🔒Dados do sistema.py

--- pages/2_🔒Dados do sistema.py
+++ pages/2_🔒Dados do sistema.py
@@ -4,9 +4,32 @@ st.set_page_config(
     page_title="Dados do Sistema",
     page_icon="♟️")
 
-st.markdown(""
-Dado do Sistema
-"")
+# jogo da adivinhação
+from random import randint
 
+print('**************************************')
+print('Seja bem vindo ao jogo de adivinhação!')
+print('**************************************')
 
+# o \n é um caracter de escape, utilizado para quebra de linha
+print('\n')
+
+numero_secreto = randint(1, 5)
+numero_escolhido = 0
+
+while True:
+    
+    try:
+        numero_escolhido = int(input('Escolha um número de 1 a 5: '))
+    except:
+        print('Você escolheu um número inválido!')
+    else:
+        if numero_escolhido not in (1, 2, 3, 4, 5):
+            print('Número precisa estar entre 1 e 5!')
+            continue
+        elif numero_escolhido == numero_secreto:
+            print(f'Parabéns! Você acertou o número secreto, já está bom né: {numero_secreto}!')
+            break
+        else:
+            print('Você errou, kkkkkkkk!')
 
